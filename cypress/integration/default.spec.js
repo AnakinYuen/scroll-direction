cc8ef99017fce30c8528cab00cc180d8037a6d78
Anakin Yuen <anakinyuen1024@gmail.com>
2020-01-15 00:22:10 +0800

    :white_check_mark: test: add tests
    testing ScrollDirection with default setting

--- cypress/integration/default.spec.js
+++ cypress/integration/default.spec.js
@@ -13,4 +13,27 @@ context('Default', function() {
   it('should have default value down in attribute', function() {
     cy.get('html').should('have.attr', 'data-scroll-direction', 'down');
   });
+
+  it('should keep the default value when going down', function() {
+    cy.smoothScrollY(0, 300)
+      .wait(100)
+      .get('html')
+      .should('have.attr', 'data-scroll-direction', 'down');
+  });
+
+  it('should change value to "up" when scroll to up', function() {
+    cy.smoothScrollY(300, 0)
+      .wait(100)
+      .get('html')
+      .should('have.attr', 'data-scroll-direction', 'up');
+  });
+
+  it('should keep the "up" value when going up', function() {
+    cy.smoothScrollY(600, 300)
+      .wait(1000)
+      .smoothScrollY(300, 0)
+      .wait(100)
+      .get('html')
+      .should('have.attr', 'data-scroll-direction', 'up');
+  });
 });
