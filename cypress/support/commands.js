e5a9fe504e3af615d3cba6879785225180f2e0d2
Anakin Yuen <anakinyuen1024@gmail.com>
2020-01-15 00:21:16 +0800

    :white_check_mark: test: add cypress command
    add cypress command for simulating user scrolling behaviour

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add(
+  'smoothScrollY',
+  { prevSubject: 'optional' },
+  (subject, from, to, duration = 1000, pixelPerScrollEvent = 5) => {
+    let target;
+    if (subject && Cypress.dom.isElement(subject)) {
+      target = Cypress.dom.unwrap(subject)[0];
+    }
+
+    cy.window().then(win => win.smoothScrollY(from, to, duration, pixelPerScrollEvent, target));
+  },
+);
